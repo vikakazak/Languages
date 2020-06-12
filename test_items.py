19b2d494faa9ecf68d7e27cb1b8780d3126d3e41
Vika Kazak <viktoria.kazak0@gmail.com>
2020-06-12 13:57:08 +0300

    change name of test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_basket(browser):
+def test_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
