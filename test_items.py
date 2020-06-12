24f9f5ca43323f2ecc63bbd7c9392c849ee4a8bf
Vika Kazak <viktoria.kazak0@gmail.com>
2020-06-12 12:27:49 +0300

    lesson 3_6

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+def test_guest_correct_answer(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    button_add_to_basket = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button_add_to_basket, "Add to basket button is not displayed"
