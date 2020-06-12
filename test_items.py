5e1fe4ccb0dba36c4190c08f0b7d3176ebb68105
Vika Kazak <viktoria.kazak0@gmail.com>
2020-06-12 13:17:20 +0300

    change name of test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_guest_correct_answer(browser):
+def test_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
